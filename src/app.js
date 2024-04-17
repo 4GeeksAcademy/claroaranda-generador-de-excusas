ef4731a8ee974ade6769948435b7c0978a06b952
Claro <aranda.claro@gmail.com>
2024-04-17 10:29:29 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,41 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getIndex(lengthArray) {
+    return Math.floor(Math.random() * lengthArray);
+  }
+
+  function getItem(someArray) {
+    return someArray[getIndex(someArray.length - 1)];
+  }
+
+  function builExcuse() {
+    return (
+      getItem(who) +
+      " " +
+      getItem(action) +
+      " " +
+      getItem(what) +
+      " " +
+      getItem(when)
+    );
+  }
+
+  const element = document.getElementById("excuse");
+
+  //element.innerText = builExcuse();
+  element.innerHTML = "<h1>" + builExcuse() + "</h1>";
+
+  //console.log(builExcuse());
 };
